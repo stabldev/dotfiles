4141cd5ed52822d8c3023fc4f8616bc3817f0ad7
tokitouq <suneeth.since2005@gmail.com>
2024-08-11 11:32:00 +0530

    chore(nvim): add neovim

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "andweeb/presence.nvim", enabled = false },
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+}
