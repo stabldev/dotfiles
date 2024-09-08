3a79aac683b101b70ef0d3ec7cdbd08b6f60a0e3
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-08 15:33:03 +0530

    chore(nvim): update configs

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,16 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "andweeb/presence.nvim", enabled = false },
   -- { "hrsh7th/nvim-cmp", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.icons", enabled = false },
+  -- { "folke/tokyonight.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  -- Completeion
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  -- { "hrsh7th/cmp-buffer", enabled = false },
+  -- { "hrsh7th/cmp-path", enabled = false },
 }
